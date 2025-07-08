ba723f8091ce3f8a348fbcdb2c51542c84e0956e
Kaic Nunes <kaicmurilo@live.com>
2025-07-08 16:27:53 -0400

    baixando video

--- tubefy.py
+++ tubefy.py
@@ -41,9 +41,8 @@ def download_file(url: str, path: Path):
     path.write_bytes(resp.content)
 
 def generate_short(title: str, assunto: str) -> tuple[Path, Path]:
-    download_dir = Path.home() / "Downloads"
-    video_path = download_dir / f"{title}.mp4"
-    thumb_path = download_dir / f"{title}.jpg"
+    base_dir = Path("videos") / title
+    base_dir.mkdir(parents=True, exist_ok=True)
 
     with sync_playwright() as p:
         ctx = p.chromium.launch_persistent_context(
@@ -52,6 +51,8 @@ def generate_short(title: str, assunto: str) -> tuple[Path, Path]:
             ignore_default_args=["--enable-automation"],
             args=["--disable-blink-features=AutomationControlled"],
             viewport={"width": 1280, "height": 800},
+            downloads_path=base_dir,
+            accept_downloads=True
         )
         page = ctx.new_page()
         page.goto("https://tubefy.io/login")
@@ -60,52 +61,101 @@ def generate_short(title: str, assunto: str) -> tuple[Path, Path]:
             page.wait_for_url("https://tubefy.io/", timeout=0)
 
         page.goto("https://tubefy.io/p_shorts")
+        print("➡️ Criando novo projeto...")
         page.click('button:has-text("Novo Projeto")')
         page.fill('div:has-text("Título") input', title)
+        print("➡️ Selecionando assunto...")
         page.select_option('select.bubble-element.Dropdown', label="Estoicismo")
+        print("➡️ Digitando prompt...")
         page.fill('div:has-text("Prompt para ideia do vídeo") textarea', assunto)
-
+        print("➡️ Avançando...")
         # Avançar duas vezes
         avancar_btn = page.locator('button:has-text("Avançar")')
         avancar_btn.wait_for(state='visible', timeout=600000)
         avancar_btn.click()
         
-        gerar_btn = page.locator('button:has-text("Gerar")')
-        gerar_btn.wait_for(state='visible', timeout=600000)
+        # print("➡️ Gerando história...")
+        # gerar_btn = page.locator('button:has-text("Gerar")')
+        # gerar_btn.wait_for(state='visible', timeout=600000)
         
-        avancar_btn_historia = page.locator('button:has-text("Avançar")')
-        avancar_btn_historia.wait_for(state='visible', timeout=600000)
-        avancar_btn_historia.click()
+        try:
+            page.wait_for_selector("text=Criando sua história...", state="visible", timeout=60000)
+            print("➡️ Aguardando História...")
+            page.wait_for_selector("text=Criando sua história...", state="hidden", timeout=120000)
+            print("➡️ História gerada com sucesso!")
+        except PlaywrightTimeoutError:
+            time.sleep(150)
+            pass
+        
+        
+        
+        
+        avancar_btn_historia = page.locator('button.baUaBaLg:has-text("Avançar")')
+        for attempt in range(3):
+            try:
+                print(f"➡️ Tentativa {attempt+1} de clicar Avançar...")
+                avancar_btn_historia.wait_for(state='visible', timeout=60000)
+                avancar_btn_historia.click()
+                break
+            except PlaywrightTimeoutError:
+                if attempt == 2:
+                    raise
+                time.sleep(5)
 
+        print("➡️ Aguardando Div Center...")
         # antes de gerar, clicar na div que contém "Center"
-        center_div = page.locator('div:has-text("Center")')
-        center_div.wait_for(state='visible', timeout=600000)
-        center_div.click()
+        page.locator('div.clickable-element:has-text("Center")').click()
+        print("➡️ Aguardando Div Center...")
         
         # Gerar Vídeo
-        gerar_video_btn = page.locator('button:has-text("Gerar")')
-        gerar_video_btn.wait_for(state='visible', timeout=600000)
-        gerar_video_btn.click()
+        print("➡️ Clicando no Botão de Gerar...")
+        page.get_by_role("button", name="Gerar").click()
         
         
+        try:
+            page.wait_for_selector("text=Criando seu vídeo....", state="visible", timeout=60000)
+            print("➡️ Aguardando Vídeo...")
+            page.wait_for_selector("text=Criando seu vídeo....", state="hidden", timeout=120000)
+            print("➡️ Vídeo gerado com sucesso!")
+        except PlaywrightTimeoutError:
+            time.sleep(150)
+            pass
+        
+        
+        # aguardar preview e baixar vídeo
+        print("➡️ Aguardando Preview...")
+        page.locator('button:has-text("Baixar Vídeo")').click()
+        page.locator('video#preview').wait_for(state='attached', timeout=600000)
+        print("➡️ Baixando Vídeo...")
+        #entra na pasta e renomeia o arquivo baixado para f"{title}.mp4" base_dir
+        time.sleep(10)
+        files = list(base_dir.iterdir())
+        file_no_ext = next((f for f in files if f.is_file() and f.suffix == ""), None)
+        if file_no_ext:
+            destino = base_dir / f"{title}.mp4"
+            file_no_ext.rename(destino)
+            print(f"✅ Vídeo renomeado para: {destino.name}")
+        else:
+            print("⚠️ Arquivo de vídeo não encontrado para renomear.")
         
 
-        # Baixar vídeo
-        page.wait_for_selector('button:has-text("Baixar Vídeo")', timeout=600000)
-        page.click('button:has-text("Baixar Vídeo")')
-        page.wait_for_selector('.short-download-link a', timeout=600000)
-        video_url = page.get_attribute('.short-download-link a', 'href')
-        download_file(video_url, video_path)
 
-        # Baixar thumbnail
-        page.click('button:has-text("Baixar Thumbnail")')
-        page.wait_for_selector('.short-download-link a', timeout=600000)
-        thumb_url = page.get_attribute('.short-download-link a', 'href')
-        download_file(thumb_url, thumb_path)
+        # baixar thumbnail
+        print("➡️ Aguardando Thumbnail...")
+        page.locator('button:has-text("Baixar Thumbnail")').click()
+        #entra na pasta e renomeia o arquivo baixado para f"{title}.png"
+        time.sleep(10)
+        files = list(base_dir.iterdir())
+        file_no_ext = next((f for f in files if f.is_file() and f.suffix == ""), None)
+        if file_no_ext:
+            destino = base_dir / f"{title}.png"
+            file_no_ext.rename(destino)
+            print(f"✅ Thumbnail renomeado para: {destino.name}")
+        else:
+            print("⚠️ Arquivo de thumbnail não encontrado para renomear.")
 
+        print("➡️ Fechando Browser...")
         ctx.close()
-
-    return video_path, thumb_path
 
 def get_json_response(script: str) -> dict:
     script = script.strip().lstrip("'\"").rstrip("'\"")
@@ -136,9 +186,7 @@ def get_json_response(script: str) -> dict:
 if __name__ == "__main__":
     script = call_ollama("llama3", PROMPT_OLLAMA)
     data = get_json_response(script)
-    title = data["title"]
+    title = data["title"] or data["titulo"]
     assunto = data["assunto"]
     assunto = TUBFY_PROMPT.replace("[ASSUNTO]", assunto)
-    video_file, thumbnail_file = generate_short(title, assunto)
-    print(f"✅ Vídeo salvo em: {video_file}")
-    print(f"✅ Thumbnail salva em: {thumbnail_file}")
+    generate_short(title, assunto)
